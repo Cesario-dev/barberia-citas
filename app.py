01c1b0ba5c6a15e12d1d9a64b1cab0b1aa17086d
Cesario-dev <cesarinoortiz@gmail.com>
2025-09-28 12:59:49 -0500

    REACTIVAR

--- app.py
+++ app.py
@@ -536,14 +536,15 @@ def ver_calendario_admin(peluquero_id):
         conn.commit()
     
     # ✅ Reactivar horario (quitar el bloqueo)
-    reactivar_dia = request.args.get('reactivar_dia')
-    reactivar_hora = request.args.get('reactivar_hora')
-    if reactivar_dia and reactivar_hora:
+    activar_dia = request.args.get("activar_dia")
+    activar_hora = request.args.get("activar_hora")
+    if activar_dia and activar_hora:
         c.execute("""
-            UPDATE horarios
-            SET bloqueado = FALSE
-            WHERE peluquero_id=%s AND dia=%s AND hora=%s
-        """, (peluquero_id, reactivar_dia, reactivar_hora))
+            INSERT INTO horarios (peluquero_id, dia, hora, bloqueado)
+            VALUES (%s, %s, %s, FALSE)
+            ON CONFLICT (peluquero_id, dia, hora)
+            DO UPDATE SET bloqueado = FALSE;
+        """, (peluquero_id, activar_dia, activar_hora))
         conn.commit()
     
     # Datos del peluquero
